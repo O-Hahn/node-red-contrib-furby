48fdcacac17a088fa04e0c1cea495dda02a0cd55
Olaf Hahn <olaf_hahn@at.ibm.com>
2016-02-16 18:48:02 +0100

    furby input

--- furby.js
+++ furby.js
@@ -214,8 +214,111 @@ module.exports = function(RED) {
 	RED.nodes.registerType("furby-output",FurbyPiOutputNode);
 
 
+    // Furby Speak Input
+	function FurbyPiInputNode(n) {
+        RED.nodes.createNode(this,n);
+        
+        this.furby = n.furby;
+        this.furbyConfig = RED.nodes.getNode(this.furby);
 
+        if (this.furbyConfig) {
+            var node = this;
+            node.tout = null;
+            var buf;
+            if (node.furbyConfig.out != "count") { buf = new Buffer(bufMaxSize); }
+            else { buf = new Buffer(Number(node.furbyConfig.newline)); }
+            var i = 0;
+            node.status({fill:"grey",shape:"dot",text:"node-red:common.status.not-connected"});
+            node.port = serialPool.get(this.serialConfig.serialport,
+                this.serialConfig.serialbaud,
+                this.serialConfig.databits,
+                this.serialConfig.parity,
+                this.serialConfig.stopbits,
+                this.serialConfig.newline
+            );
 
+            var splitc;
+            if (node.furbyConfig.newline.substr(0,2) == "0x") {
+                splitc = new Buffer([parseInt(node.furbyConfig.newline)]);
+            } else {
+                splitc = new Buffer(node.furbyConfig.newline.replace("\\n","\n").replace("\\r","\r").replace("\\t","\t").replace("\\e","\e").replace("\\f","\f").replace("\\0","\0")); // jshint ignore:line
+            }
+
+            this.port.on('data', function(msg) {
+                // single char buffer
+                if ((node.furbyConfig.newline === 0)||(node.furbyConfig.newline === "")) {
+                    if (node.furbyConfig.bin !== "bin") { node.send({"payload": String.fromCharCode(msg)}); }
+                    else { node.send({"payload": new Buffer([msg])}); }
+                }
+                else {
+                    // do the timer thing
+                    if (node.furbyConfig.out === "time") {
+                        if (node.tout) {
+                            i += 1;
+                            buf[i] = msg;
+                        }
+                        else {
+                            node.tout = setTimeout(function () {
+                                node.tout = null;
+                                var m = new Buffer(i+1);
+                                buf.copy(m,0,0,i+1);
+                                if (node.furbyConfig.bin !== "bin") { m = m.toString(); }
+                                node.send({"payload": m});
+                                m = null;
+                            }, node.furbyConfig.newline);
+                            i = 0;
+                            buf[0] = msg;
+                        }
+                    }
+                    // count bytes into a buffer...
+                    else if (node.furbyConfig.out === "count") {
+                        buf[i] = msg;
+                        i += 1;
+                        if ( i >= parseInt(node.furbyConfig.newline)) {
+                            var m = new Buffer(i);
+                            buf.copy(m,0,0,i);
+                            if (node.furbyConfig.bin !== "bin") { m = m.toString(); }
+                            node.send({"payload":m});
+                            m = null;
+                            i = 0;
+                        }
+                    }
+                    // look to match char...
+                    else if (node.furbyConfig.out === "char") {
+                        buf[i] = msg;
+                        i += 1;
+                        if ((msg === splitc[0]) || (i === bufMaxSize)) {
+                            var n = new Buffer(i);
+                            buf.copy(n,0,0,i);
+                            if (node.furbyConfig.bin !== "bin") { n = n.toString(); }
+                            node.send({"payload":n});
+                            n = null;
+                            i = 0;
+                        }
+                    }
+                }
+            });
+            this.port.on('ready', function() {
+                node.status({fill:"green",shape:"dot",text:"node-red:common.status.connected"});
+            });
+            this.port.on('closed', function() {
+                node.status({fill:"red",shape:"ring",text:"node-red:common.status.not-connected"});
+            });
+        } else {
+            this.error(RED._("serial.errors.missing-conf"));
+        }
+
+        this.on("close", function(done) {
+            if (this.furbyConfig) {
+                serialPool.close(this.furbyConfig.serialport,done);
+            } else {
+                done();
+            }
+        });
+    }
+    RED.nodes.registerType("furby-input",FurbyPiInputNode);
+
+    // SerialPool holds the serial communication to the device
     var serialPool = (function() {
         var connections = {};
         return {
